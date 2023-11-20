909465bb3ee718cd068799037209e4667e17bff7
mariusjacob-batstateu <21-07121@g.batstate-u.edu.ph>
2023-11-20 23:07:07 +0800

    player draft commit
    check na sa joystick
    ongoing ang shoot

---
+++ src/Scripts/Player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@onready var joystick = $'../joystick'
+
+
+var speed=300
+func _physics_process(delta):
+	var direction= joystick.posVector
+	if direction:
+		velocity=direction*speed
+	else:
+		velocity=Vector2(0,0)
+	move_and_slide()
