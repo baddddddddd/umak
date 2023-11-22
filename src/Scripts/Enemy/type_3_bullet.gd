12ec2dc504ae849d7516cbcb3bfa3375446e3a99
JonBabao <jonendrickb@gmail.com>
2023-11-22 13:13:18 +0800

    type3bullettest

--- src/Scripts/Enemy/type_3_bullet.gd
+++ src/Scripts/Enemy/type_3_bullet.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var suction_speed = 100
 var entered = false
 var target_body
+var shake_threshold = 10.0
+var shake_time_threshold = 0.5
+var elapsed_time_since_shake = 0.0
 
 func _ready():
 	$AnimationPlayer.play("Whirlpool")
@@ -12,9 +15,16 @@ func _ready():
 func _process(delta):
 	if entered:
 		var direction = (self.global_position - target_body.global_position).normalized()
-		
 		target_body.global_position += direction * suction_speed * delta
-	
+		var acceleration = Input.get_accelerometer()
+		var acceleration_length = acceleration.length()
+		
+		if acceleration_length > shake_threshold:
+			if acceleration_length > shake_threshold:
+				elapsed_time_since_shake += delta
+				if elapsed_time_since_shake > shake_time_threshold:
+					suction_speed = 0
+					elapsed_time_since_shake = 0.0
 	
 func _physics_process(delta):
 	velocity.x = -speed
@@ -25,8 +35,6 @@ func _on_area_2d_body_entered(body):
 	if body.has_method("shoot"):
 		target_body = body
 		entered = true
-		
-
 
 func _on_area_2d_body_exited(body):
 	if entered:
