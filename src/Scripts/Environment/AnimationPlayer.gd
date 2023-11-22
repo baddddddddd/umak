a33e9aedba36fba23b2c4f6102f8dc32f5c9fd6e
mariusjacob-batstateu <21-07121@g.batstate-u.edu.ph>
2023-11-23 02:27:56 +0800

    cutscene, sorry late naaaaaa
    btw ang dependencies baka mag not located:
    AnimatedPlayer - nasa script, environment
    1,2,3,4,5,6,7, banner, black - nasa Assets/CutScene
    
    thankssss

---
+++ src/Scripts/Environment/AnimationPlayer.gd
@@ -0,0 +1,13 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play("pan1")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
