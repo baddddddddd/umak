edc53a77df439741c66dd3f74b30b0f494894a37
JonBabao <jonendrickb@gmail.com>
2023-11-23 03:23:34 +0800

    Update type_5_bullet.gd

--- src/Scripts/Enemy/type_5_bullet.gd
+++ src/Scripts/Enemy/type_5_bullet.gd
@@ -1,12 +1,19 @@
 extends RigidBody2D
 
-@export var speed = 200
-@export var damage = 10
+@export var speed = 50
+@export var damage = 100
+@onready var state = 0
 
 func _physics_process(delta):
-	linear_velocity = Vector2(-speed, 0)
 	
-
+	if state == 0:
+		print("up")
+		linear_velocity = Vector2(speed, -100)
+		state = 1
+	elif state == 1:
+		print("down")
+		linear_velocity = Vector2(speed, 100)
+		state = 0
 
 func _on_body_entered(body):
 	if body.is_in_group("friendly"):
