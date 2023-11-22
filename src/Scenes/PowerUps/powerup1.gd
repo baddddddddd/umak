20ffa9e6a206da12452fc73733f563ce9ed5fbf6
JonBabao <jonendrickb@gmail.com>
2023-11-22 15:10:06 +0800

    PowerUp

---
+++ src/Scenes/PowerUps/powerup1.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 40
+
+func _ready():
+	$AnimationPlayer.play("Powerup1")
+	
+func _physics_process(delta):
+	velocity.x = -speed
+	
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("friendly"):
+		queue_free()
--- src/Scenes/StageOne.gd
+++ src/Scenes/StageOne.gd
@@ -5,6 +5,7 @@ var rng = RandomNumberGenerator.new()
 var my_random_number = rng.randf_range(-10.0, 10.0)
 
 @export var enemies: Array[PackedScene]
+@export var powerups: Array[PackedScene]
 
 @onready var spawn_area = $SpawnArea/CollisionShape2D
 @onready var pause_menu = $PauseMenu
@@ -12,6 +13,7 @@ var my_random_number = rng.randf_range(-10.0, 10.0)
 var paused = true
 
 @onready var spawn_timer = $EnemySpawnClock
+@onready var powerup_timer = $PowerUpSpawnClock
 @onready var type_4 = preload("res://Scripts/Enemy/type_4.tscn")
 
 var top_left = Vector2(0, 0)
@@ -21,93 +23,109 @@ var top_left = Vector2(0, 0)
 
 
 func _ready():
-	top_left.x = spawn_area.global_position.x - (spawn_area.shape.size.x * 0.5)
-	top_left.y = spawn_area.global_position.y - (spawn_area.shape.size.y * 0.5)
-	
-	pause_menu.hide()
-	game_over_screen.hide()
-	
-	start_wave()
-	
-	await get_tree().create_timer(10.0).timeout
-	
-	var question = "What is my name?"
-	var choices = [
-		"Vlad",
-		"Blado",
-		"Lad"
-	]
-	var answer = "Vladimir"
-	
-	trigger_qna(question, choices, answer)
-	
+    top_left.x = spawn_area.global_position.x - (spawn_area.shape.size.x * 0.5)
+    top_left.y = spawn_area.global_position.y - (spawn_area.shape.size.y * 0.5)
+    
+    pause_menu.hide()
+    game_over_screen.hide()
+    
+    start_wave()
+    
+    await get_tree().create_timer(10.0).timeout
+    
+    var question = "What is my name?"
+    var choices = [
+        "Vlad",
+        "Blado",
+        "Lad"
+    ]
+    var answer = "Vladimir"
+    
+    trigger_qna(question, choices, answer)
+    
 
 func _on_enemy_spawn_clock_timeout():
-	var number_of_enemies = rng.randi_range(1, 2)
-	
-	for i in range(number_of_enemies):
-		spawn_enemy()
-		await get_tree().create_timer(1).timeout
+    var number_of_enemies = rng.randi_range(1, 2)
+    
+    for i in range(number_of_enemies):
+        spawn_enemy()
+        await get_tree().create_timer(1).timeout
+
+func _on_power_up_spawn_clock_timeout():
+    spawn_powerup()
+    await get_tree().create_timer(5).timeout
 
 
 func trigger_qna(question, choices, answer):
-	spawn_timer.stop()
-	await get_tree().create_timer(3.0).timeout
-	
-	var correct_answer = rng.randi_range(1, 4)
-	
-	for i in range(1, 5):
-		var y_position = top_left.y + (spawn_area.shape.size.y * (0.20 * i))
-		var x_position = top_left.x
-		
-		var enemy_instance = type_4.instantiate()
-		enemy_instance.global_position = Vector2(x_position, y_position)
-		
-		var banner_text = ""
-		
-		if i == correct_answer:
-			enemy_instance.set("invincible", false)
-			banner_text = answer
-		else:
-			banner_text = choices.pop_front()
-			
-		get_tree().current_scene.add_child(enemy_instance)
-	
-	
+    spawn_timer.stop()
+    await get_tree().create_timer(3.0).timeout
+    
+    var correct_answer = rng.randi_range(1, 4)
+    
+    for i in range(1, 5):
+        var y_position = top_left.y + (spawn_area.shape.size.y * (0.20 * i))
+        var x_position = top_left.x
+        
+        var enemy_instance = type_4.instantiate()
+        enemy_instance.global_position = Vector2(x_position, y_position)
+        
+        var banner_text = ""
+        
+        if i == correct_answer:
+            enemy_instance.set("invincible", false)
+            banner_text = answer
+        else:
+            banner_text = choices.pop_front()
+            
+        get_tree().current_scene.add_child(enemy_instance)
+    
+    
 func start_wave():
-	spawn_timer.start()
-	
+    spawn_timer.start()
+    powerup_timer.start()
+    
 
 func spawn_enemy():
-	var random_y = rng.randf_range(top_left.y, top_left.y + spawn_area.shape.size.y)
-	
-	var random_type = rng.randi_range(0, enemies.size() - 1)
-	
-	var enemy = enemies[random_type].instantiate()
-	enemy.global_position = Vector2(top_left.x, random_y)
-	
-	get_tree().current_scene.add_child(enemy)
+    var random_y = rng.randf_range(top_left.y, top_left.y + spawn_area.shape.size.y)
+    
+    var random_type = rng.randi_range(0, enemies.size() - 1)
+    
+    var enemy = enemies[random_type].instantiate()
+    enemy.global_position = Vector2(top_left.x, random_y)
+    
+    get_tree().current_scene.add_child(enemy)
 
-
+func spawn_powerup():
+    var random_y = rng.randf_range(top_left.y, top_left.y + spawn_area.shape.size.y)
+    
+    var random_type = rng.randi_range(0, enemies.size() - 1)
+    
+    var powerup = powerups[random_type].instantiate()
+    powerup.global_position = Vector2(top_left.x, random_y)
+    
+    get_tree().current_scene.add_child(powerup)
+    
 func _on_despawn_body_exited(body):
-	if body.get_meta("entity_type") in ["bullet", "enemy"]:
-		body.queue_free()
-		
+    if body.get_meta("entity_type") in ["bullet", "enemy"]:
+        body.queue_free()
+        
 func pause():
-	if paused:
-		pause_menu.show()
-		Engine.time_scale = 0
-	else:
-		pause_menu.hide()
-		Engine.time_scale = 1
-		
-	paused = !paused
+    if paused:
+        pause_menu.show()
+        Engine.time_scale = 0
+    else:
+        pause_menu.hide()
+        Engine.time_scale = 1
+        
+    paused = !paused
 
 
 func end_game():
-	Engine.time_scale = 0
-	game_over_screen.show()
-	
+    Engine.time_scale = 0
+    game_over_screen.show()
+    
 
 func _on_pause_button_pressed():
-	pause()
+    pause()
+
+
