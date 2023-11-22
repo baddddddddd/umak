e425912f9777b954cfb2607635d1407b9c303aeb
Eluxe <eluxeofficial@gmail.com>
2023-11-22 14:47:56 +0800

    Add qna wave

--- src/Scenes/StageOne.gd
+++ src/Scenes/StageOne.gd
@@ -13,6 +13,7 @@ var paused = true
 
 @onready var spawn_timer = $EnemySpawnClock
 @onready var type_4 = preload("res://Scripts/Enemy/type_4.tscn")
+@onready var banner_ui = preload("res://Scenes/banner.tscn")
 
 var top_left = Vector2(0, 0)
 
@@ -29,7 +30,7 @@ func _ready():
 	
 	start_wave()
 	
-	await get_tree().create_timer(10.0).timeout
+	await get_tree().create_timer(4.0).timeout
 	
 	var question = "What is my name?"
 	var choices = [
@@ -50,28 +51,44 @@ func _on_enemy_spawn_clock_timeout():
 		await get_tree().create_timer(1).timeout
 
 
+func show_banner(text):
+	hud.get_node("MarginContainer/RichTextLabel").text = text
+	
+	
+
 func trigger_qna(question, choices, answer):
 	spawn_timer.stop()
 	await get_tree().create_timer(3.0).timeout
+	
+	show_banner(question)
 	
 	var correct_answer = rng.randi_range(1, 4)
 	
 	for i in range(1, 5):
 		var y_position = top_left.y + (spawn_area.shape.size.y * (0.20 * i))
 		var x_position = top_left.x
-		
-		var enemy_instance = type_4.instantiate()
-		enemy_instance.global_position = Vector2(x_position, y_position)
-		
-		var banner_text = ""
+		var position = Vector2(x_position, y_position)
 		
 		if i == correct_answer:
-			enemy_instance.set("invincible", false)
-			banner_text = answer
+			spawn_type_4(position, answer, false)
 		else:
-			banner_text = choices.pop_front()
+			var banner_text = choices.pop_front()
+			spawn_type_4(position, banner_text, true)
 			
-		get_tree().current_scene.add_child(enemy_instance)
+			
+	
+
+func spawn_type_4(position, banner_text, invincible):
+	var enemy_instance = type_4.instantiate()
+	enemy_instance.global_position = position
+	enemy_instance.set("invincible", invincible)
+	
+	var banner = enemy_instance.get_node("Banner")
+	var banner_text_ui = banner.get_node("MarginContainer/RichTextLabel")
+	banner_text_ui.text = banner_text
+	
+	get_tree().current_scene.add_child(enemy_instance)
+	banner.show()
 	
 	
 func start_wave():
