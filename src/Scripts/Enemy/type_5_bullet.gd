eb6f52cc8378a823ce2a0113857e3281cd2d35a5
JonBabao <jonendrickb@gmail.com>
2023-11-23 13:43:54 +0800

    type5and6

--- src/Scripts/Enemy/type_5_bullet.gd
+++ src/Scripts/Enemy/type_5_bullet.gd
@@ -1,18 +1,20 @@
 extends RigidBody2D
 
 @export var speed = 50
-@export var damage = 100
+@export var damage = 20
 @onready var state = 0
 
 func _physics_process(delta):
 	
 	if state == 0:
 		print("up")
-		linear_velocity = Vector2(speed, -100)
+		linear_velocity = Vector2(-speed, -10)
+		await get_tree().create_timer(1).timeout
 		state = 1
 	elif state == 1:
 		print("down")
-		linear_velocity = Vector2(speed, 100)
+		linear_velocity = Vector2(-speed, 10)
+		await get_tree().create_timer(1).timeout
 		state = 0
 
 func _on_body_entered(body):
